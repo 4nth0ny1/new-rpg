14b2a11a3c12612ba01a6a8b935720248f644547
4nth0ny1 <acatullo4@gmail.com>
2021-10-27 22:31:15 -0500

    add checkWin to game logic

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ export default class App extends React.Component {
     this.state = {
       turn: false,
       count: 0,
-      message: 'no need to reshuffle',
-      playerOneHealth: 1000,
-      playerTwoHealth: 1000, 
+      message: ' ',
+      playerOneHealth: 100,
+      playerTwoHealth: 100, 
       playerOneAttack: 0,
       playerTwoAttack: 0,
       playerOneDefense: 0, 
       playerTwoDefense: 0,
       cards: [
         { id: 7, name: 'rifle', image: "https://images.unsplash.com/photo-1525779350160-30931cbc2aac?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJpZmxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
-        { id: 8, name: 'revive 10 health', image: "https://images.unsplash.com/photo-1563260324-5ebeedc8af7c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lZCUyMGtpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: 5  },
+        { id: 8, name: 'medkit', image: "https://images.unsplash.com/photo-1563260324-5ebeedc8af7c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lZCUyMGtpdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: 5  },
         { id: 9, name: 'forest base', image: "https://images.unsplash.com/photo-1455496231601-e6195da1f841?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9yZXN0JTIwYmFzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
         { id: 10, name: 'spear', image: "https://images.unsplash.com/photo-1628329752902-cf567df10ed0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3BlYXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
         { id: 11, name: 'lightning', image: "https://images.unsplash.com/photo-1429552077091-836152271555?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGlnaHRuaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
@@ -31,7 +31,7 @@ export default class App extends React.Component {
         { id: 3, name: 'laser', image: "https://images.unsplash.com/photo-1481241857164-e8483bce922d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bGF6ZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
       ],
       player_two_hand: [
-        { id: 4, name: 'mountain base', image: "https://images.unsplash.com/photo-1570793005386-840846445fed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW4lMjBodXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
+        { id: 4, name: 'mountain', image: "https://images.unsplash.com/photo-1570793005386-840846445fed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91bnRhaW4lMjBodXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
         { id: 5, name: 'excalibur', image: "https://images.unsplash.com/photo-1440711085503-89d8ec455791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3dvcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
         { id: 6, name: 'shield', image: "https://images.unsplash.com/photo-1561156772-a44477f220a5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hpZWxkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", attack: Math.floor(Math.random() * 15), defense: Math.floor(Math.random() * 10)  },
       ],
@@ -44,12 +44,12 @@ export default class App extends React.Component {
     let pOne = this.state.player_one_hand
     let pTwo = this.state.player_two_hand
     let cards = this.state.cards
-    let disCards = this.state.discards
+    // let disCards = this.state.discards
     let pOneHealth = this.state.playerOneHealth
     let pTwoHealth = this.state.playerTwoHealth
 
-    const itCards = cards.map((k,v) => <div>{k.name}</div>)
-    const itDiscards = disCards.map((k,v) => <div>{k.name}</div>)
+    // const itCards = cards.map((k,v) => <div>{k.name}</div>)
+    // const itDiscards = disCards.map((k,v) => <div>{k.name}</div>)
       
     const itOne = pOne.map((k, v) => 
       <div className="card">
@@ -96,9 +96,22 @@ export default class App extends React.Component {
     /// --------------------------------------------
       /// --------------------------------------------
 
+    const checkWin = () => {
+      if (this.state.playerOneHealth < 0 || this.state.playerTwoHealth < 0) {
+        if (this.state.playerOneHealth > this.state.playerTwoHealth) {
+          this.setState({
+            message: 'Player One Won'
+          })
+        } else {
+          this.setState({
+            message: 'Player Two Won'
+          })
+        }
+      }
+    }
 
     const addCardandSwitchTurn = () => {
-      
+      checkWin()
       let randomIndex = Math.floor(Math.random() * cards.length)
       let randomCard = cards[randomIndex]
       cards.splice(randomIndex, 1)
@@ -120,8 +133,7 @@ export default class App extends React.Component {
       } else if (this.state.cards.length === 0) {
         this.setState({
           cards: [...this.state.discards, randomCard],
-          discards: [{}],
-          message: 'pick a card'
+          discards: [{}]
         })
       }
     }
@@ -132,8 +144,8 @@ export default class App extends React.Component {
           <div className='player-container top'>
             <div className='player-id'>
               <h1>Player One</h1>
-              <p>{this.state.playerOneHealth}</p>
-              <p>My Turn: {!this.state.turn ? "true" : "false"}</p>
+              <p>Health: {this.state.playerOneHealth}</p>
+              <p>{this.state.turn ? "My Turn" : " "}</p>
               <p>Attack: {sumOneAttack}</p>
               <p>Defense: {sumOneDefense}</p>
             </div>
@@ -141,13 +153,14 @@ export default class App extends React.Component {
           </div>
           <div className="game-board">
               <button onClick={addCardandSwitchTurn}>draw card and attack</button>
+              {this.state.message}
           </div>
           <div className='player-container bottom'>
             <div className="hand">{itTwo}</div>
             <div className='player-id'>
               <h1>Player Two</h1>
-              <p>{this.state.playerTwoHealth}</p>
-              <p>My Turn: {this.state.turn ? "true" : "false"}</p>
+              <p>Health: {this.state.playerTwoHealth}</p>
+              <p>{!this.state.turn ? "My Turn" : " "}</p>
               <p>Attack: {sumTwoAttack}</p>
               <p>Defense: {sumTwoDefense}</p>
             </div>
